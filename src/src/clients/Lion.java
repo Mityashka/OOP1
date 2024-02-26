a2d78031ae7634cabba42f9e8191c3e5ad8b60c0
Mityashka <dima.kuzkin@yanex.ru>
2024-02-26 17:59:52 +0300

    сделал дз

---
+++ src/src/clients/Lion.java
@@ -0,0 +1,14 @@
+package clients;
+
+import java.time.LocalDate;
+
+public class Lion extends Animal {
+    public Lion(String name, float weight, LocalDate age, Owner owner) {
+        super(name, weight, age, owner);}
+        protected void swim(){
+            System.out.println(getType() + " cannot swim");
+        }
+        protected void fly(){
+            System.out.println(getType() + " cannot fly");
+        }
+}
