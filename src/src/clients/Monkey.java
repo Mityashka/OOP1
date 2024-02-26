e7d0d5a6de5dc99db25ffb59d837050397048353
Mityashka <122776197+Mityashka@users.noreply.github.com>
2024-02-26 18:04:33 +0300

    Добавил файлы
    Добавил файлики, так как не разобрался как пушить через идею

---
+++ src/src/clients/Monkey.java
@@ -0,0 +1,12 @@
+package clients;
+
+import java.time.LocalDate;
+
+public class Monkey extends Animal {
+    public Monkey(String name, float weight, LocalDate age, Owner owner) {
+        super(name, weight, age, owner);
+    }
+    protected void fly(){
+        System.out.println(getType() + " cannot fly");
+    }
+}
